ca16be804004289a9f00d2e3de8091b914d6159d
Slava <vyacheslav.fomenko@attractgroup.com>
2018-10-03 19:43:25 +0300

    gitignore

--- nginx-proxy-bridge.sh
+++
@@ -1,3 +0,0 @@
-docker stop nginx-proxy
-docker rm -f nginx-proxy
-docker run -d -p 80:80 -p 5000:5000 -v /var/run/docker.sock:/tmp/docker.sock:ro -v ~/docker-proxy/nginx.conf:/etc/nginx/nginx.conf --name nginx-proxy --restart=always jwilder/nginx-proxy
